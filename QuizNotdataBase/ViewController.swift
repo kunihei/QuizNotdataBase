58e1d75c0632887f7239bccd1cd624acef3e9aae
kunihei <57933493+kunihei@users.noreply.github.com>
2021-05-24 18:59:31 +0900

    Initial Commit

---
+++ QuizNotdataBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizNotdataBase
+//
+//  Created by 祥平 on 2021/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
